1de2a5d326067f36f61180870d0a3a705f243e11
Akimba-png <akimba@gmail.com>
2021-06-03 10:57:14 +0300

    Добавляет и отрисовывает компонент карточки на главной странице. Добавляет валидацию props в компонентах.

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainPage from './../main-page/main-page';
 
-function App() {
+function App(props) {
+  const {placeCardsCount} = props;
   return (
-    <MainPage />
+    <MainPage placeCardsCount={placeCardsCount}/>
   );
 }
+
+App.propTypes = {
+  placeCardsCount: PropTypes.number.isRequired,
+};
 
 export default App;
