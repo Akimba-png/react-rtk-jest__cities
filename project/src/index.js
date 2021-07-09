a67ef0bc38e5681ad92d8ca8acc4589d972b34b2
Akimba-png <akimba@gmail.com>
2021-07-09 21:07:45 +0300

    Выносит создание store в отдельный модуль

--- project/src/index.js
+++ project/src/index.js
@@ -1,32 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './components/app/app';
-import { reducer } from './store/reducer';
 import { reviews } from './mocks/reviews';
-import { createApi } from './services/api';
 import { fetchOffersList, checkAuth } from './store/api-actions';
-import { ActionCreator } from './store/action';
-import { AuthorizationStatus } from './const';
-import { redirect } from './store/middlewares/redirect';
-
-const api = createApi(
-  () => store.dispatch(
-    ActionCreator
-      .requireAuthorization(AuthorizationStatus.NOT_AUTH),
-  ),
-);
-
-const store = createStore(
-  reducer,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api)),
-    applyMiddleware(redirect),
-  ),
-);
+import { store } from './store/store';
 
 store.dispatch(checkAuth());
 store.dispatch(fetchOffersList());
