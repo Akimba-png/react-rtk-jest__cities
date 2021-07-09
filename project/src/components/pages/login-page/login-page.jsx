d1cef8552c7fbac33df22ebf9ed39b7ab457335f
Akimba-png <akimba@gmail.com>
2021-07-09 07:08:27 +0300

    Добавляет валидацию формы страницы авторизации

--- project/src/components/pages/login-page/login-page.jsx
+++ project/src/components/pages/login-page/login-page.jsx
@@ -1,26 +1,30 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import { connect } from 'react-redux';
 import Logo from './../../logo/logo';
 import Navigation from './../../navigation/navigation';
 import { login } from './../../../store/api-actions';
+import { AuthorizationStatus, AppRoute } from './../../../const';
+
+const VALIDITY_MESSAGE = 'Это небезопасный пароль, добавьте символ отличный от пробела';
 
 function LoginPage(props) {
-  const { onFormSubmit } = props;
+  const { onFormSubmit, currentAuthorizationStatus } = props;
+  const { register, handleSubmit, getValues, formState: { errors } } = useForm();
 
-  const loginInputRef = useRef(null);
-  const passwordInputRef = useRef(null);
+  if (currentAuthorizationStatus === AuthorizationStatus.AUTH) {
+    return <Redirect to={AppRoute.MAIN} />;
+  }
 
-
-  const handleLoginFormSubmit = (evt) => {
-    evt.preventDefault();
+  const handleLoginFormSubmit = () => {
     const formValue = {
-      email: loginInputRef.current.value,
-      password: passwordInputRef.current.value,
+      email: getValues().email,
+      password: getValues().password,
     };
     onFormSubmit(formValue);
   };
-
 
   return (
     <div className="page page--gray page--login">
@@ -37,14 +41,15 @@ function LoginPage(props) {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form onSubmit={handleLoginFormSubmit} className="login__form form" action="#" method="post">
+            <form onSubmit={handleSubmit(handleLoginFormSubmit)} className="login__form form" action="#" method="post">
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input ref={loginInputRef} className="login__input form__input" type="email" name="email" placeholder="Email" required="required" />
+                <input {...register('email')} className="login__input form__input" type="email" name="email" placeholder="Email" required="required" />
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input ref={passwordInputRef} className="login__input form__input" type="password" name="password" placeholder="Password" required="required" />
+                {errors.password && <span>{VALIDITY_MESSAGE}</span>}
+                <input {...register('password', { pattern: /\S/ })} className="login__input form__input" type="password" name="password" placeholder="Password" required="required" />
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
@@ -64,6 +69,7 @@ function LoginPage(props) {
 
 LoginPage.propTypes = {
   onFormSubmit: PropTypes.func.isRequired,
+  currentAuthorizationStatus: PropTypes.string.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => ({
@@ -72,5 +78,9 @@ const mapDispatchToProps = (dispatch) => ({
   },
 });
 
+const mapStateToProps = (state) => ({
+  currentAuthorizationStatus: state.authorizationStatus,
+});
+
 export { LoginPage };
-export default connect(null, mapDispatchToProps)(LoginPage);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
