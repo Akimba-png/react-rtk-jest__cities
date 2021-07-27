886d654217c24a7f3f1a3952b3a325ae082ba0c8
Akimba-png <akimba@gmail.com>
2021-07-27 22:54:22 +0300

    Добавляет в FavoritePage логику навигации, рефакторит фильтр и сортировку

--- project/src/components/location-list/location-list.jsx
+++ project/src/components/location-list/location-list.jsx
@@ -9,7 +9,10 @@ function LocationList() {
   const dispatch = useDispatch();
 
   const handleLinkClick = (location) =>
-    () => dispatch(changeCity(location));
+    (evt) => {
+      evt.preventDefault();
+      dispatch(changeCity(location));
+    };
 
   return (
     <ul className="locations__list tabs__list" data-testid="location-list">
@@ -17,7 +20,11 @@ function LocationList() {
         const keyValue = `${index}-${location}`;
         return (
           <li className="locations__item" key={keyValue}>
-            <a onClick={handleLinkClick(location)} className={`locations__item-link tabs__item ${location === city ? 'tabs__item--active' : ''}`} href="/#">
+            <a
+              onClick={handleLinkClick(location)}
+              className={`locations__item-link tabs__item ${location === city ? 'tabs__item--active' : ''}`}
+              href="/#"
+            >
               <span>{location}</span>
             </a>
           </li>
